f1358f66b7a6dc8e31ec24c72e395169d3b298be
EdwinMakRouts <makoveevedwin@gmail.com>
2025-09-09 07:18:40 +0200

    CORS

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 import models
 import routes
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+# Configuración de CORS
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["http://localhost:4200"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 app.include_router(routes.country_router)
 app.include_router(routes.league_router)
