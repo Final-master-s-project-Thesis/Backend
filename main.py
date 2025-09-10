fe8b40169712d31422c20f4b422327faac216e1c
EdwinMakRouts <makoveevedwin@gmail.com>
2025-09-11 00:45:06 +0200

    Fix CORS policy

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ import routes
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
+origins = [
+	"http://localhost:4200",
+    "https://srff-c341a.web.app", 
+]
+
 # Configuración de CORS
 app.add_middleware(
-	CORSMiddleware,
-	allow_origins=["http://localhost:4200"],
-	allow_credentials=True,
-	allow_methods=["*"],
-	allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(routes.country_router)
